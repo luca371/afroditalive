8d8f5854e95d6c206d1c5ff5a5628ada384488f0
Luca-Andrei Serban <E092557@adgroup.michelin.com>
2026-04-18 10:27:41 +0300

    fix webhook raw body

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LandingScreen    from './screens/LandingScreen';
-import LoginScreen      from './screens/LoginScreen';
-import RegisterScreen   from './screens/RegisterScreen';
-import OnboardingScreen from './screens/OnboardingScreen';
-import DashboardScreen  from './screens/DashboardScreen';
-import BookingScreen    from './screens/BookingScreen';
-import ProtectedRoute   from './components/ProtectedRoute';
-import PublicRoute      from './components/PublicRoute';
+import LandingScreen       from './screens/LandingScreen';
+import LoginScreen         from './screens/LoginScreen';
+import RegisterScreen      from './screens/RegisterScreen';
+import OnboardingScreen    from './screens/OnboardingScreen';
+import DashboardScreen     from './screens/DashboardScreen';
+import BookingScreen       from './screens/BookingScreen';
+import CancelBookingScreen from './screens/CancelBookingScreen';
+import ProtectedRoute      from './components/ProtectedRoute';
+import PublicRoute         from './components/PublicRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Public */}
-        <Route path="/"                element={<LandingScreen />} />
-        <Route path="/book/:salonSlug" element={<BookingScreen />} />
+        <Route path="/"                    element={<LandingScreen />} />
+        <Route path="/book/:salonSlug"     element={<BookingScreen />} />
+        <Route path="/cancel/:bookingId"   element={<CancelBookingScreen />} />
 
         {/* Doar pentru neautentificați */}
         <Route path="/login" element={
